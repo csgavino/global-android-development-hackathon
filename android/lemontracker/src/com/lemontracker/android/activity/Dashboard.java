bd6f88a8a6a656810e326799f543854506d2b3b0
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-17 23:27:32 +0800

    Set up actionbar layout
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- android/lemontracker/src/com/lemontracker/android/activity/Dashboard.java
+++ android/lemontracker/src/com/lemontracker/android/activity/Dashboard.java
@@ -1,15 +1,32 @@
 package com.lemontracker.android.activity;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 import com.lemontracker.android.R;
 
+@EActivity
 public class Dashboard extends Activity {
+
+    @ViewById
+    TextView lemonTextView;
+
+    @ViewById
+    TextView trackerTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_layout);
+        lemonTextView.setText("Lemon");
+        Typeface tf = Typeface.createFromAsset(getAssets(),
+                "FuturaLT.ttf");
+        lemonTextView.setTypeface(tf);
+        trackerTextView.setText("Tracker");
+        trackerTextView.setTypeface(tf);
     }
 
 }
