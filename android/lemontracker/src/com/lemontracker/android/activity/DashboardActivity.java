e70b348d3a13cf1498f749b42386ce4b35d00d56
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-18 18:32:02 +0800

    Use image instead of using a custom font to render the logo
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- android/lemontracker/src/com/lemontracker/android/activity/DashboardActivity.java
+++ android/lemontracker/src/com/lemontracker/android/activity/DashboardActivity.java
@@ -2,33 +2,18 @@ package com.lemontracker.android.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.TextView;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.ViewById;
 import com.lemontracker.android.R;
 
 @EActivity
 public class DashboardActivity extends Activity {
 
-    @ViewById
-    TextView lemonTextView;
-
-    @ViewById
-    TextView trackerTextView;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_layout);
-        lemonTextView.setText("Lemon");
-        Typeface tf = Typeface.createFromAsset(getAssets(),
-                "FuturaLT-CondensedBold.ttf");
-        lemonTextView.setTypeface(tf);
-        trackerTextView.setText("Tracker");
-        trackerTextView.setTypeface(tf);
     }
 
     @Click(R.id.listButton)
