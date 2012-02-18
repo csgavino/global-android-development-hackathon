a4390641da21dce29064bda4c5a69ae2265a2394
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-18 22:55:01 +0800

    Initial parties activity
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

---
+++ android/lemontracker/src/com/lemontracker/android/activity/PartiesActivity.java
@@ -0,0 +1,14 @@
+package com.lemontracker.android.activity;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.lemontracker.android.base.BaseListActivity_;
+
+import static com.lemontracker.android.WebService.*;
+
+@EActivity
+public class PartiesActivity extends BaseListActivity_ {
+    @Override
+    protected String service() {
+        return events(4L);
+    }
+}
