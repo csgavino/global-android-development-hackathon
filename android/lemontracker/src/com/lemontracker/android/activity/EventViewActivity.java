d2c0eb0de4e1323dfb835f2f322b13accb9a3b3a
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-18 16:58:20 +0800

    Set labels based on passed extra
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- android/lemontracker/src/com/lemontracker/android/activity/EventViewActivity.java
+++ android/lemontracker/src/com/lemontracker/android/activity/EventViewActivity.java
@@ -2,20 +2,62 @@ package com.lemontracker.android.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.Extra;
+import com.googlecode.androidannotations.annotations.UiThread;
+import com.googlecode.androidannotations.annotations.ViewById;
 import com.lemontracker.android.R;
+import com.lemontracker.android.model.Event;
+import com.teamcodeflux.android.Result;
 
-@EActivity
+@EActivity(R.layout.event_layout)
 public class EventViewActivity extends Activity {
+
+    @Extra("event")
+    Event event;
+    @ViewById
+    TextView category;
+    @ViewById
+    TextView h1;
+    @ViewById
+    TextView h2;
+    @ViewById
+    TextView description;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.event_layout);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        retrieveEntry();
+    }
+
+    @AfterViews
+    public void afterCreate() {
+        h1.setText(event.getName());
+        h2.setText(event.getDateStart().toString());
+        description.setText(event.getDescription());
     }
 
     @Click(R.id.map)
     public void mapCellClicked() {
     }
+
+    @Background
+    public void retrieveEntry() {
+
+    }
+
+    @UiThread
+    public void processResult(Result result) {
+
+    }
+
 }
