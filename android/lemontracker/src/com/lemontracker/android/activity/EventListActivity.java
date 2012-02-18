01f66ff9d8dd6de73439d0f2f40da10176a7685d
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-18 16:00:16 +0800

    Use activity instead of list activity
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- android/lemontracker/src/com/lemontracker/android/activity/EventListActivity.java
+++ android/lemontracker/src/com/lemontracker/android/activity/EventListActivity.java
@@ -1,21 +1,21 @@
 package com.lemontracker.android.activity;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ItemClick;
 import com.googlecode.androidannotations.annotations.UiThread;
+import com.googlecode.androidannotations.annotations.ViewById;
 import com.lemontracker.android.R;
-import com.lemontracker.android.model.Category;
 import com.lemontracker.android.model.Event;
-import com.lemontracker.android.model.User;
 import com.lemontracker.android.util.EventArrayAdapter;
 import com.teamcodeflux.android.Result;
 import org.springframework.web.client.RestClientException;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static com.lemontracker.android.WebService.*;
@@ -23,44 +23,22 @@ import static com.teamcodeflux.android.RestTemplateFactory.*;
 import static java.util.Arrays.*;
 
 @EActivity
-public class EventListActivity extends ListActivity {
+public class EventListActivity extends Activity {
     protected static final String TAG = EventListActivity.class.getSimpleName();
     protected List<Event> events;
 
+    @ViewById(R.id.__list)
+    ListView list;
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_layout);
+    }
 
-        events = new ArrayList<Event>();
-
-        for (int i = 0; i < 5; i++) {
-
-            Category category = Category.buildCategory(
-                    "name " + i
-            );
-
-            User user = User.buildUser(
-                    "name " + i
-            );
-
-            Event event = Event.buildEvent(
-                    "name " + i,
-                    "blurb " + i,
-                    "description " + i,
-                    "imageURL " + i,
-                    "thumbnailURL " + i,
-                    new Date(),
-                    new Date(),
-                    category,
-                    user
-            );
-
-            events.add(event);
-        }
-
-        ArrayAdapter adapter = new EventArrayAdapter(this, R.layout.list_cell_layout, events);
-        setListAdapter(adapter);
-
+    @Override
+    public void onResume() {
+        super.onResume();
         retrieveEntries();
     }
 
@@ -76,15 +54,20 @@ public class EventListActivity extends ListActivity {
     }
 
     @UiThread
-    void processResult(Result result) {
+    public void processResult(Result result) {
         if (result.hasErrors()) {
             EventListActivity.this.finish();
         } else {
             List __events = (ArrayList<Event>) result.getResult();
             ArrayAdapter adapter = new EventArrayAdapter(this, R.layout.list_cell_layout, __events);
-            setListAdapter(adapter);
+            list.setAdapter(adapter);
         }
     }
 
-
+    /*
+    @ItemClick(R.id.__list)
+    public void listCellClicked(Event event) {
+        System.out.println("Hello");
+    }
+    */
 }
