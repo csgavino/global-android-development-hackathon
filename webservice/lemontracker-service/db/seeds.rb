37db127f466fe329cabea6eecc14555da27855c6
Kimson Wong <kimsonwong@teamcodeflux.com>
2012-02-18 15:28:06 +0800

    Added sample event for Shopping

--- webservice/lemontracker-service/db/seeds.rb
+++ webservice/lemontracker-service/db/seeds.rb
@@ -18,7 +18,7 @@ Event.create( :name => "SM City Iloilo 3 Day Sale",
               :description => "March 18-20, 2012 -  SM City Iloilo 3-day SALE!  Get up to 70% off on great selections! All SM Advantage and BDO Rewards card holders will get additional 10% OFF* at SM City Iloilo from 10am to 12mn!", 
               :blurb => "Get up to 70% off on great selections!",
               :date_start => DateTime.civil(2012, 3, 18, 10, 0, 0).in_time_zone('Asia/Taipei'), 
-              :date_end => DateTime.civil(2012, 3, 20, 11, 59, 59).in_time_zone('Asia/Taipei'),
+              :date_end => DateTime.civil(2012, 3, 20, 23, 59, 59).in_time_zone('Asia/Taipei'),
               :thumb_url =>  "/images/thumbs/1.jpg", 
               :image_url =>  "/images/event_banners/1.jpg", 
               :longitude => 122.550473, 
@@ -30,7 +30,7 @@ Event.create( :name => "SM North Edsa 3 Day Sale",
               :description => "February 3-5, 2012 - SM North Edsa 3-day SALE!  Get up to 70% off on great selections! All SM Advantage and BDO Rewards card holders will get additional 10% OFF* at the 2-Hour Special on Friday February 3, 2011 at SM North EDSA from 10am to 12nn!", 
               :blurb => "Get up to 70% off on great selections!",
               :date_start => DateTime.civil(2012, 2, 17, 10, 0, 0).in_time_zone('Asia/Taipei'), 
-              :date_end => DateTime.civil(2012, 2, 19, 11, 59, 59).in_time_zone('Asia/Taipei'), 
+              :date_end => DateTime.civil(2012, 2, 19, 23, 59, 59).in_time_zone('Asia/Taipei'), 
               :thumb_url => "/images/thumbs/2.jpg", 
               :image_url => "/images/event_banners/2.jpg", 
               :longitude => 121.031249, 
@@ -47,4 +47,29 @@ Event.create( :name => "DEATH CAB for CUTIE Concert",
               :longitude => 121.049359, 
               :latitude => 14.548735, 
               :category_id => 1)
+              
+Event.create( :name => "PENSHOPPE Trinoma Sale", 
+              :description => "Enjoy up to 30% off on Penshoppe's!  ALL items are on sale from April 10-12, 2012! 30% off on selected women's styles, 20% off on selected men's styles and 30% off on all body sprays.", 
+              :blurb => "Enjoy up to 30% off on Penshoppe's!  ALL items are on sale from April 10-12, 2012!",
+              :date_start => DateTime.civil(2012,4, 10, 10, 0, 0).in_time_zone('Asia/Taipei'), 
+              :date_end => DateTime.civil(2012, 4, 12, 23, 59, 59).in_time_zone('Asia/Taipei'), 
+              :thumb_url => "/images/thumbs/4.jpg", 
+              :image_url => "/images/event_banners/4.jpg", 
+              :longitude => 121.033523,
+              :latitude => 14.653015, 
+              :category_id => 3)
+
+Event.create( :name => "Status Magazine Yard Sale", 
+              :description => "Status Magazine yard sale at Embassy in The Fort, Taguig on August 1, 2012. Entrance fee is Php100 and the sale is open from 11am to 7pm.", 
+              :blurb => "Status Magazine yard sale at Embassy in The Fort, Taguig on August 1, 2012.",
+              :date_start => DateTime.civil(2012, 8, 1, 11, 0, 0).in_time_zone('Asia/Taipei'), 
+              :date_end => DateTime.civil(2012, 8, 1, 19, 0, 0).in_time_zone('Asia/Taipei'), 
+              :thumb_url => "/images/thumbs/5.jpg", 
+              :image_url => "/images/event_banners/5.jpg", 
+              :longitude => 121.047894,
+              :latitude => 14.549794, 
+              :category_id => 3)
+              
+
+              
                       
