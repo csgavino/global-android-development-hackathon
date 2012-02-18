58182f722e8900ee40c6d7c2f2c1d3c1e2bcb4e4
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-19 02:43:36 +0800

    Initial radius activity
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

---
+++ android/lemontracker/src/com/lemontracker/android/activity/RadiusActivity.java
@@ -0,0 +1,16 @@
+package com.lemontracker.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.lemontracker.android.R;
+
+@EActivity(R.layout.radius_layout)
+public class RadiusActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
