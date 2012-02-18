933b66862bc8a8d1494293f91488edd5829c10a9
Kimson Wong <kimsonwong@teamcodeflux.com>
2012-02-18 17:14:30 +0800

    Added sample content for concerts

--- webservice/lemontracker-service/db/seeds.rb
+++ webservice/lemontracker-service/db/seeds.rb
@@ -70,6 +70,27 @@ Event.create( :name => "Status Magazine Yard Sale",
               :latitude => 14.549794, 
               :category_id => 3)
               
+Event.create( :name => "Avril Lavigne Concert", 
+              :description => "Canadian singer Avril Lavigne is coming back to Manila for The Black Star tour to rock again the stage of Smart Araneta Coliseum on February 19, 2012. Avril Lavigne better known by her birth name of Avril Ramona Lavigne born in September 27, 1984 is a Canadian Grammy award-nominated pop punk singer, musician, fashion designer and actress. In 2006, Canadian Business Magazine ranked her the seventh most powerful Canadian in Hollywood. Having sold close to 30 million albums worldwide, Avril Lavigne is quickly paving her own path to rock n roll royalty.  Three multi-platinum albums, Let Go, Under My Skin and Best Damn Thing Lavigne scored a string of international hit singles: Complicated, Sk8er Boi, Im With You, Losing Grip, Dont Tell Me, My Happy Ending, Nobodys Home, Keep Holding On, Girlfriend, When Youre Gone, Hot, and The Best Damn Thing. She earned eight Grammy Award nominations for her debut album Let Go and has sold 16 Million copies worldwide. She also won seven Canadian Juno Awards, and sold nearly 20 million tracks worldwide over the past eight years. Her 2004 sophomore effort, Under My Skin debuted at #1 around the world, as did her much anticipated third album, Best Damn Thing.  Shes coming back with her brand new hit album Goodbye Lullaby including smash hits What The Hell, Smile and Wish You Were Here. Dont miss The Black Star Tour: Avril Lavigne live in Manila on February 19, 2012. Limited VIP Tickets Call Wilbros Live at 374-2222
+              TICKET PRICES:
+              Patron Center  P7920
+              Patron Sides  P6340
+              Lower Box  P5280
+              Upper Box A  P2640
+              Upper Box B  P1585
+              Gen Admission  P530
+
+              For ticket inquiries you may call Wilbros Live! 374 2222 or Ticketnet 911 5555
+
+              Brought to you by Wilbros Live!", 
+              :blurb => "Avril Lavigne Black Star Tour",
+              :date_start => DateTime.civil(2012, 2, 19, 20, 0, 0).in_time_zone('Asia/Taipei'), 
+              :date_end => DateTime.civil(2012, 2, 19, 23, 0, 0).in_time_zone('Asia/Taipei'), 
+              :thumb_url => "/images/thumbs/6.jpg", 
+              :image_url => "/images/event_banners/6.jpg", 
+              :longitude => 121.053451,
+              :latitude => 14.620748, 
+              :category_id => 1)   
 
               
                       
