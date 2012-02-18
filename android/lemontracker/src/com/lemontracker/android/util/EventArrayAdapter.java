0dc603c59b558cbb11295b672465bec6f4aaabe2
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-18 12:17:14 +0800

    Created custom event cell array adapter
    This is the second attempt at creating a custom adapter for displaying the views hopefully this'll give us what we want.
    
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

---
+++ android/lemontracker/src/com/lemontracker/android/util/EventArrayAdapter.java
@@ -0,0 +1,56 @@
+package com.lemontracker.android.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.lemontracker.android.R;
+import com.lemontracker.android.model.Event;
+
+import java.util.List;
+
+public class EventArrayAdapter extends ArrayAdapter<Event> {
+
+    public EventArrayAdapter(Context context, int id, List<Event> events) {
+        super(context, id, events);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            LayoutInflater inflater = (LayoutInflater) this.getContext().
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.list_cell_layout, null);
+        }
+
+        Event event = getItem(position);
+
+        /*
+        ImageView icon = (ImageView) row.findViewById(R.id.listIcon);
+        Bitmap bitmap = resource(subcat.getName());
+        icon.setImageBitmap(bitmap);
+        */
+
+        TextView name = (TextView) row.findViewById(R.id.header);
+        name.setText("Hello");
+
+        TextView blurb = (TextView) row.findViewById(R.id.blurb);
+        blurb.setText("Swedes");
+
+        TextView date = (TextView) row.findViewById(R.id.date);
+        date.setText("Vikings");
+
+        return row;
+    }
+
+    private Bitmap resource(String name) {
+        return null;
+    }
+
+}
+
+
