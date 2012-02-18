c434ea976caaec7d932917d16b0c956deaa64e27
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-18 22:57:21 +0800

    Start concerts activity on click
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- android/lemontracker/src/com/lemontracker/android/activity/ConcertsActivity.java
+++ android/lemontracker/src/com/lemontracker/android/activity/ConcertsActivity.java
@@ -1,9 +1,11 @@
 package com.lemontracker.android.activity;
 
+import com.googlecode.androidannotations.annotations.EActivity;
 import com.lemontracker.android.base.BaseListActivity_;
 
 import static com.lemontracker.android.WebService.*;
 
+@EActivity
 public class ConcertsActivity extends BaseListActivity_ {
 
     @Override
--- android/lemontracker/src/com/lemontracker/android/activity/DashboardActivity.java
+++ android/lemontracker/src/com/lemontracker/android/activity/DashboardActivity.java
@@ -143,6 +143,7 @@ public class DashboardActivity extends Activity {
 
     @Click(R.id.concerts)
     public void concertsCellClicked() {
+        Intent i = new Intent(this, ConcertsActivity_.class);
     }
 
     @Click(R.id.parties)
