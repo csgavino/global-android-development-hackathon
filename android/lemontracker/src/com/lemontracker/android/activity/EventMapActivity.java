2854d103a2cfca2315d8ef869731e14f41520a07
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-02-19 03:37:52 +0800

    Use android annotations on the map activity
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- android/lemontracker/src/com/lemontracker/android/activity/EventMapActivity.java
+++ android/lemontracker/src/com/lemontracker/android/activity/EventMapActivity.java
@@ -2,19 +2,19 @@ package com.lemontracker.android.activity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
+import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.ViewById;
 import com.lemontracker.android.MapItemizedOverlay;
 import com.lemontracker.android.R;
 
 import java.util.List;
 
+@EActivity(R.layout.map_layout)
 public class EventMapActivity extends MapActivity {
     public static final String TAG = EventMapActivity.class.getSimpleName();
 
@@ -23,15 +23,13 @@ public class EventMapActivity extends MapActivity {
     Drawable drawable;
     MapView mapview;
 
-    /*
-    @ViewById
-    MapView mapview;
-    */
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_layout);
+    }
+
+    @AfterViews
+    public void afterCreate() {
         mapview = (MapView) findViewById(R.id.mapview);
         mapview.setBuiltInZoomControls(true);
         mapOverlays = mapview.getOverlays();
